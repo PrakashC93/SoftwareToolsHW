f5a18e0b2b3d0e7c831b8935b5f5b48d8301ca06
Prakash_Chinnasamy <prakash.chinnasamy@student.tu-freiberg.de>
2019-06-29 12:58:52 +0200

    final versions

--- gravity_task2_3D.py
+++ gravity_task2_3D.py
@@ -10,19 +10,20 @@ GRAVITATIONAL_CONSTANT = 6.67430e-11  # m^3 kg^-1 s^-2
 pos = np.array([[-1, 0], [1, 0]])
 vel = np.array([[0, -1], [0, 1]])
 nob = len(pos)                          #no of bodies in space
+pos = np.reshape(pos, (nob,2,1))
+vel = np.reshape(vel, (nob,2,1))
 mass = np.array([4 / GRAVITATIONAL_CONSTANT, 4 / GRAVITATIONAL_CONSTANT])
 TIME_STEP = 0.0001  # s
 NUMBER_OF_TIME_STEPS = 1000000
 PLOT_INTERVAL = 1000
-POSI = np.reshape(pos, (nob,2,1))
 
 #Main Execution
-                                                    #an empty array to store the accelerations of all planets
+ac_s = np.zeros((nob,2,1))                                                    #an empty array to store the accelerations of all planets
 
 for step in tqdm(range(NUMBER_OF_TIME_STEPS + 1)):                          #repeating for every time
     # if step % PLOT_INTERVAL == 0:
     #     fig, ax = plt.subplots()
-    #     ax.scatter(pos[:,0], pos[:,1])
+    #     ax.scatter(pos[:,0,step], pos[:,1,step])
     #     ax.set_aspect("equal")
     #     ax.set_xlim(-1.5, 1.5)
     #     ax.set_ylim(-1.5, 1.5)
@@ -39,20 +40,16 @@ for step in tqdm(range(NUMBER_OF_TIME_STEPS + 1)):                          #rep
             if i==j:                                                       #to not calculate for itself
                 continue
             
-            dist_vect = pos[j,:]-pos[i,:]                        #x2-x1 and y2-y1 are stored in this vector
+            dist_vect = pos[j,:,step]-pos[i,:,step]                        #x2-x1 and y2-y1 are stored in this vector
             dist = np.sqrt(np.sum(dist_vect**2))                           #distance between points given by sqrt of sum of x2-x1 and y2-y1
             acc = (GRAVITATIONAL_CONSTANT*mass[j]*dist_vect)/(dist**2)     #acceleration of body due to force
-            acceleration = np.append([acceleration, acc])                  #acceleration of all bodies for current time step
-    acceleration = np.reshape(acceleration, (nob,2))                       #reshaping to make it homogenous with data structure
-    #ac_s = acceleration                                 #appending current accelration to acc matrix
-    temppos = pos[:,:]+(vel[:,:]*TIME_STEP)                      #Euler method to find new position and velocity
-    POSI = np.dstack([POSI, temppos])
-    pos = temppos
-    new_vel = vel[:,:]+(acceleration[:,:]*TIME_STEP)
-    vel = new_vel
-
-#print(POSI)
-
+            acceleration = np.hstack([acceleration, acc])                  #acceleration of all bodies for current time step
+    acceleration = np.reshape(acceleration, (nob,2,1))                       #reshaping to make it homogenous with data structure
+    ac_s = np.dstack([ac_s, acceleration])                                 #appending current accelration to acc matrix
+    new_pos = pos[:,:,step]+(vel[:,:,step]*TIME_STEP)                      #Euler method to find new position and velocity
+    pos = np.dstack([pos, new_pos])
+    new_vel = vel[:,:,step]+(ac_s[:,:,step+1]*TIME_STEP)
+    vel = np.dstack([vel, new_vel])
 
 
 
--- gravity_task3d.py
+++ gravity_task3d.py
@@ -1,4 +1,9 @@
 #using numpy arrays
+
+#time taken for given 1000000 iterations is 27m 3s(avg of 3 runs)
+#iterations reduced to 100000 for more manageable levels 
+
+#importing required libraries
 import numpy as np 
 from pathlib import Path
 import matplotlib.pyplot as plt
@@ -9,53 +14,53 @@ from tqdm import tqdm
 GRAVITATIONAL_CONSTANT = 6.67430e-11  # m^3 kg^-1 s^-2
 pos = np.array([[-1, 0], [1, 0]])
 vel = np.array([[0, -1], [0, 1]])
-pos = np.reshape(pos, (2,2,1))
-vel = np.reshape(vel, (2,2,1))
+nob = len(pos)                          #no of bodies in space
+nod = 2                                 #no of dimensions
 mass = np.array([4 / GRAVITATIONAL_CONSTANT, 4 / GRAVITATIONAL_CONSTANT])
-TIME_STEP = 0.1  # s
-NUMBER_OF_TIME_STEPS = 10
-PLOT_INTERVAL = 1
+TIME_STEP = 0.001  # s
+NUMBER_OF_TIME_STEPS = 100000
+PLOT_INTERVAL = 100
+POSI = np.reshape(pos, (nob,2,1))
 
-ac_s = np.zeros((2,2,1))
-for step in tqdm(range(NUMBER_OF_TIME_STEPS + 1)):
-    if step % PLOT_INTERVAL == 0:
-        fig, ax = plt.subplots()
-        ax.scatter(pos[:,0,step], pos[:,1,step])
-        ax.set_aspect("equal")
-        ax.set_xlim(-1.5, 1.5)
-        ax.set_ylim(-1.5, 1.5)
-        ax.set_title("t = {:8.4f} s".format(step * TIME_STEP))
-        ax.set_xlabel("x")
-        ax.set_ylabel("y")
-        output_file_path = Path("positions", "{:016d}.png".format(step))
-        output_file_path.parent.mkdir(exist_ok=True)
-        fig.savefig(output_file_path)
-        plt.close(fig)
-    acceleration = np.array([])
-    for i in range(len(pos)):
-        #acceleration = np.array([])
-        for j in range(len(pos)):
-            if i==j:
+#asserting
+
+assert len(pos) == len(vel) == len(mass)
+assert len(pos[:]) == nod 
+assert len(vel[:]) == nod
+
+#Main Execution
+
+
+for step in tqdm(range(NUMBER_OF_TIME_STEPS + 1)):                          #repeating for every time
+    # if step % PLOT_INTERVAL == 0:
+    #     fig, ax = plt.subplots()
+    #     ax.scatter(pos[:,0], pos[:,1])
+    #     ax.set_aspect("equal")
+    #     ax.set_xlim(-1.5, 1.5)
+    #     ax.set_ylim(-1.5, 1.5)
+    #     ax.set_title("t = {:8.4f} s".format(step * TIME_STEP))
+    #     ax.set_xlabel("x")
+    #     ax.set_ylabel("y")
+    #     output_file_path = Path("positions", "{:016d}.png".format(step))
+    #     output_file_path.parent.mkdir(exist_ok=True)
+    #     fig.savefig(output_file_path)
+    #     plt.close(fig)
+    acceleration = np.array([])                                            #an empty numpy array to store the acceleration of bodies for current time step
+    for i in range(nob):
+        for j in range(nob):
+            if i==j:                                                       #to not calculate for itself
                 continue
             
-            dist_vect = pos[j,:,step]-pos[i,:,step]
-            dist = np.sqrt(np.sum(dist_vect**2))
-            acc = (GRAVITATIONAL_CONSTANT*mass[j]*dist_vect)/(dist**2)
-            acceleration = np.hstack([acceleration, acc])
-    acceleration = np.reshape(acceleration, (2,2,1))
-    ac_s = np.dstack([ac_s, acceleration])
-    new_vel = vel[:,:,step]+(ac_s[:,:,step+1]*TIME_STEP)                
-    vel = np.dstack([vel, new_vel])
-    new_pos = pos[:,:,step]+(vel[:,:,step+1]*TIME_STEP)             #semi implicit euler method
-    pos = np.dstack([pos, new_pos])
-print(pos)
-plt.plot(pos[0,0,:],pos[0,1,:])
-plt.plot(pos[1,0,:],pos[1,1,:])
-plt.show()
-
-
-
-
-
-
+            dist_vect = pos[j,:]-pos[i,:]                                  #x2-x1 and y2-y1 are stored in this vector
+            dist = np.sqrt(np.sum(dist_vect**2))                           #distance between points given by sqrt of sum of x2-x1 and y2-y1
+            acc = (GRAVITATIONAL_CONSTANT*mass[j]*dist_vect)/(dist**2)     #acceleration of body due to force
+            acceleration = np.hstack([acceleration, acc])                #acceleration of all bodies for current time step
+    acceleration = np.reshape(acceleration, (nob,2))                       #reshaping to make it homogenous with data structure
+    
+    # Semi implicit Euler method to find new position and velocity
+    new_vel = vel[:,:]+(acceleration[:,:]*TIME_STEP)
+    vel = new_vel
+    temppos = pos[:,:]+(vel*TIME_STEP)
+    POSI = np.dstack([POSI, temppos])
+    pos = temppos
 
